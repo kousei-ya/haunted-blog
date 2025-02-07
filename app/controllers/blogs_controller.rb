7bcdbdab2fd2ae4467dfabdc0f8a9ed57c50ad69
kousei-ya <k.yano@genbasupport.com>
2025-02-07 18:06:11 +0900

    プライベートな投稿を閲覧できる場合の条件を追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,11 @@ class BlogsController < ApplicationController
     @blogs = Blog.search(params[:term]).published.default_order
   end
 
-  def show; end
+  def show
+    return unless @blog.secret? && @blog.user != current_user
+
+    raise ActiveRecord::RecordNotFound, '権限がありません'
+  end
 
   def new
     @blog = Blog.new
