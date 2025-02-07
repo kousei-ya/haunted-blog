a6a2a060fd1320e6a10b1ead123d5bdc450115f2
kousei-ya <k.yano@genbasupport.com>
2025-02-07 18:28:49 +0900

    APIのレスポンスにメールアドレスが含まれないように修正

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -3,8 +3,9 @@
 class Api::LikingUsersController < ApplicationController
   def index
     blog = Blog.find(params[:blog_id])
-    @users = blog.liking_users.order(:id)
+    @users = blog.liking_users.order(:id).as_json(only: %i[id nickname])
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
+    render json: { users: @users, destroy_path: @destroy_path }
   end
 end
