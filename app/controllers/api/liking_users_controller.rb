7a73de8a15b123787a5c0532e4aca30c687e08da
kousei-ya <k.yano@genbasupport.com>
2025-02-13 10:13:31 +0900

    APIのレスポンスから個人情報が分かる問題について別の方法で対策するように修正

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -3,9 +3,8 @@
 class Api::LikingUsersController < ApplicationController
   def index
     blog = Blog.find(params[:blog_id])
-    @users = blog.liking_users.order(:id).as_json(only: %i[id nickname])
+    @users = blog.liking_users.order(:id)
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
-    render json: { users: @users, destroy_path: @destroy_path }
   end
 end
